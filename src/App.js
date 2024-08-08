d5df3ba23a03f011ec51e0158f0ce371441fb5a4
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-08 22:59:44 +0530

    Added a xml structure in App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
     </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input type="text" placeholder="🖊️ Add item..." />
+      <i className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+      <div className="todo">
+        <div className="left">
+          <input type="checkbox" name="" id="" />
+          <p>Rect tutorial</p>
+        </div>
+        <div className="right">
+          <i className="fas fa-times"></i>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
