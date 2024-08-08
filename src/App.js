e1e028ae42ae5978727f04227cc2c662fb12025c
Muhammed Fairooz <fairooz2150@gmail.com>
2024-08-08 23:33:25 +0530

    Added mapping function for listing To Do lists

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
 import './App.css'
 import { useState } from 'react';
 function App() {
-  const [toDos,setToDos] = useState([])
+  const [toDos, setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+
   return (
     <div className="app">
-    <div className="mainHeading">
-      <h1>ToDo List</h1>
-    </div>
-    <div className="subHeading">
-      <br />
-      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-    </div>
-    <div className="input">
-      <input type="text" placeholder="🖊️ Add item..." />
-      <i className="fas fa-plus"></i>
-    </div>
-    <div className="todos">
-      <div className="todo">
-        <div className="left">
-          <input type="checkbox" name="" id="" />
-          <p>Rect tutorial</p>
-        </div>
-        <div className="right">
-          <i className="fas fa-times"></i>
-        </div>
+
+      <div className="mainHeading">
+        <h1>To Do List</h1>
       </div>
+
+      <div className="subHeading">
+        <br/>
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
+      </div>
+
+      <div className="todos">
+
+      {  toDos.map((value)=>{
+
+        return ( <div className="todo">
+
+            <div className="left">
+              <input type="checkbox" name="" id="" />
+              <p>{value}</p>
+            </div>
+
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+
+        </div> )
+        }) }
+      </div>
+
     </div>
-  </div>
   );
 }
 
